9c87896bfa6869ff735b0efe8b96b83a1686c11f
fhemery <f.hemery@hemit.fr>
2023-12-08 15:36:24 +0100

    feat: ping
    Implements issue #1 and #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'quizzy-front-root',
+  imports: [RouterModule, HeaderComponent],
+  selector: 'qzy-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { PingService, PingStatus } from '../../services/ping.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@Component({
+  selector: 'qzy-header',
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatTooltipModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  private readonly pingService = inject(PingService);
+  pingStatus$ = this.pingService.ping();
+  protected readonly PingStatus = PingStatus;
+}
