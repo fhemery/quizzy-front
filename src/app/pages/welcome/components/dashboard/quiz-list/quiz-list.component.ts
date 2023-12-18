6a77baaa561965ce999879aec960d89a2bc16859
fhemery <f.hemery@hemit.fr>
2023-12-18 16:41:20 +0100

    refactor: extra quizz button for reuse in quizz list

--- src/app/pages/welcome/components/dashboard/quiz-list/quiz-list.component.ts
+++ src/app/pages/welcome/components/dashboard/quiz-list/quiz-list.component.ts
@@ -5,11 +5,12 @@ import { TranslateModule } from '@ngx-translate/core';
 import { QuizListResponse } from '../../../../../services/quiz.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { NewQuizButtonComponent } from '../new-quiz-button/new-quiz-button.component';
 
 @Component({
   selector: 'qzy-quiz-list',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, TranslateModule, MatIconModule, MatTooltipModule],
+  imports: [CommonModule, MatButtonModule, TranslateModule, MatIconModule, MatTooltipModule, NewQuizButtonComponent],
   templateUrl: './quiz-list.component.html',
   styleUrl: './quiz-list.component.scss',
 })
